0220f8ae187b2ea1f31dc6a49993441b051ca858
Patryk Adach <patrykadachofficial@gmail.com>
2021-03-05 12:16:43 +0100

    Created Articles functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d4869cf49cd6fc7c9335";
+/******/ 	var hotCurrentHash = "ec6b4a12dfac62343772";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head' // it's something similar to <head> in HTML, you can use custom meta tags and title and so on
 import styles from '../styles/Layout.module.css'
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
-  return (
+export default function Home({articles}) {
+
+  	return (
     <div>
       <Head>
         <title>Crypto News</title>
@@ -10,6 +12,19 @@ export default function Home() {
       </Head>
 
       <h1>Welcome to NEXT </h1>
+      <ArticleList articles={articles} />
     </div>
   )
+}
+
+// getStaticProps, how to fetch data 
+export const getStaticProps = async () => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+	const articles = await res.json();
+
+	return {
+		props: {
+			articles
+		}
+	}
 }
